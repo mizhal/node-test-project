76413002a71458051398794b9b7d83daa7a332c2
mizhal <mizhal@gmail.com>
2015-06-17 10:48:32 +0200

    Instalar extension virtual-fields de sequelize

--- server/components/sequelize_singleton/index.js
+++ server/components/sequelize_singleton/index.js
@@ -4,6 +4,7 @@
 var sequelize_db_configs = require("../../config/database.json");
 var current_config = sequelize_db_configs[process.env.NODE_ENV];
 var Sequelize = require('sequelize');
+require('sequelize-virtual-fields')(Sequelize);
 var sequelize = new Sequelize(current_config.database, current_config.username, current_config.password, {
   host: current_config.host,
   port: current_config.port,
