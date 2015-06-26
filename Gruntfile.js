ebd9c23d9e1b0fd96fc68d54b4c93918c11c0fe8
mizhal <mizhal@gmail.com>
2015-06-26 13:04:50 +0200

    Añadida tarea grunt wiredep para actualizar las dependencias de bower en karma. Excluido angular-scenario que no es compatible con karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -242,6 +242,22 @@ module.exports = function (grunt) {
         src: '<%= yeoman.client %>/index.html',
         ignorePath: '<%= yeoman.client %>/',
         exclude: [/bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/ ]
+      },
+      testKarma: {
+          src: ['karma.conf.js'],
+          fileTypes: {
+            js: {
+              block: /(([\s\t]*)\/\/\s*bower:*(\S*))(\n|\r|.)*?(\/\/\s*endbower)/gi,
+              detect: {
+                js: /'(.*\.js)'/gi
+              },
+              replace: {
+                js: '\'{{filePath}}\','
+              }
+            }
+          },
+          devDependencies: true,
+          exclude: [/bootstrap-sass-official/, /jasmine/]
       }
     },
 
