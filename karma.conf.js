99984cad7ed6208ecb95a3c7c23143f5a52ea547
mizhal <mizhal@gmail.com>
2015-07-03 12:26:54 +0200

    Lanzar karma en modo testing continuo

--- karma.conf.js
+++ karma.conf.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
@@ -93,7 +93,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS', 'Chrome'],
 
 
     // Continuous Integration mode
