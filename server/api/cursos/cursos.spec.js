1f39dffb5c65fabd3ad71bbaf3b04a4e8bfd9119
mizhal <mizhal@gmail.com>
2015-07-14 13:20:29 +0200

    Tests de cursos

--- server/api/cursos/cursos.spec.js
+++ server/api/cursos/cursos.spec.js
@@ -23,10 +23,11 @@ describe('GET /api/cursos', function() {
       });
   });
 
-  it("should make crud", function(done){
+  it("should make crud and generate slug", function(done){
     var NOMBRE_CURSO = "Curso 2015/2016";
     join(
       // destruir los cursos creados por tests previos
+      // el join es necesario porque el map lleva promesas
       Curso.findAll({
         where: {nombre: NOMBRE_CURSO}
       }).map(function(curso){
@@ -39,19 +40,34 @@ describe('GET /api/cursos', function() {
       Curso.create({
         nombre: NOMBRE_CURSO,
         anyo: 2015
-      }).then(function(curso){
-
-        Curso.findAll({
+      }).then(function(){
+        // ver que esta guardado
+        return Curso.findAll({
           where: {
             nombre: NOMBRE_CURSO
           }
-        }).then(function(found){
-          found.length.should.be.equal(1);
+        }).then(function(cursos){
+          // probar la generacion del slug
+          cursos.length.should.be.equal(1);
+          cursos[0].slug.should.be.eql("Curso-20152016-2015");
+          return cursos;
+        }).then(function(cursos){
+          // probar actualizacion
+          var curso = cursos[0];
+          curso.nombre = "Otro nombre";
+          return curso.save();
+        }).then(function(curso){
+          // probar actualizacion del slug
+          curso.slug.should.be.eql("Otro-nombre-2015");
+          return curso;
+        }).then(function(curso){
+          // probar destroy
+          return curso.destroy();
+        }).then(function(){
           done();
         });
 
       });
-
 
     });
 
