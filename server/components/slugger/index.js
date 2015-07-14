fb195b8da4ee9c7a94e260f9fc010f34f4100ed3
mizhal <mizhal@gmail.com>
2015-07-14 12:38:50 +0200

    Fix: slug era variable global; Usar un valor directo en vez de una callback para generar el slug

--- server/components/slugger/index.js
+++ server/components/slugger/index.js
@@ -13,15 +13,8 @@ var Promise = require('bluebird');
     fila/objeto concreta que contiene el slug
   @param Model 
     tipo de datos/tabla/modelo
-  @param slug_source_fields_callback 
-    Callback/lambda/funcion que devuelve la cadena que se usa para calcular el slug, por ejemplo
-    se puede usar la suma de otros campos o formulas mas complejas como
-      function(){
-        if(curso.anyo < 2011)
-          return curso.nombre + "antiguo"
-        else
-          return curso.nombre + "-" + curso.anyo
-      }
+  @param source_value 
+    texto que se usara para generar el slug
   @param slug_field_name 
     Nombre del campo que contiene el slug en la base de datos
   @param slug_field_size 
@@ -32,18 +25,17 @@ var Promise = require('bluebird');
 var generator = function(
   instance, // fila concreta que contiene el slug
   Model, // tipo de datos/tabla/modelo
-  slug_source_fields_callback, 
+  source_value, 
   slug_field_name, 
   slug_field_size,
   repeats
 ){
-  var source = slug_source_fields_callback();
   if(repeats == null) {
     repeats = "";
   }
 
-  if(source) {
-    slug = slug_lib(source);
+  if(source_value) {
+    var slug = slug_lib(source_value);
     if(slug.length + repeats.length > slug_field_size) {
       slug = slug.slice(0, slug_field_size - repeats.length - 1);
     }
@@ -54,7 +46,7 @@ var generator = function(
       }).then(function(count){
         if(repeats == "") repeats = 0;
         if(count > 0){
-          return generator(instance, Model, slug_source_fields_callback,
+          return generator(instance, Model, source_value,
             slug_field_name, slug_field_size, repeats + 1);
         } else {
           instance[slug_field_name] = slug;
