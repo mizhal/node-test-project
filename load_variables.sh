d775e0fdfb354ac6389bf260290507970f4a4986
mizhal <mizhal@gmail.com>
2015-07-16 10:48:12 +0200

    Alias para regenerar la base de datos de prueba

--- load_variables.sh
+++ load_variables.sh
@@ -4,3 +4,4 @@
 CURRENT_PATH=`pwd`
 export PATH=$PATH:$CURRENT_PATH/node_modules/.bin/
 export NODE_ENV=development
+alias regenerate-test-db="node admin-utils/drop_database.js test; node admin-utils/create_database.js test; NODE_ENV=test sequelize db:migrate"
