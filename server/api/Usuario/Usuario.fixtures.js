0dfb269c0aeaa9d5e0f46be09f0e9b893741619c
mizhal <mizhal@gmail.com>
2015-07-16 14:21:18 +0200

    Test de asignacion de roles a usuarios

---
+++ server/api/Usuario/Usuario.fixtures.js
@@ -0,0 +1,57 @@
+/** FIXTURES for "Usuario" endpoint **/
+module.exports = {
+  "admin-credentials": {
+    login: "admin",
+    password: "admin"
+  },
+  "admin-2": {
+    login: "admin-2", 
+    password: "admin",
+    ultimo_acceso: 1,
+    puede_entrar: true
+  },
+  "usuario-1": {
+    login: "usuario-1",
+    password: "the-password",
+    ultimo_acceso: 1,
+    puede_entrar: true
+  },
+  "usuario-2": {
+    login: "usuario-2",
+    password: "the-password",
+    ultimo_acceso: 1,
+    puede_entrar: true
+  },
+  "usuario-3": {
+    login: "usuario-3",
+    password: "the-password",
+    ultimo_acceso: 1,
+    puede_entrar: true
+  },
+  "test-1": {
+    login: "test1",
+    password: "the-password",
+    ultimo_acceso: 1,
+    puede_entrar: true
+  },
+  "test-2": {
+    login: "test2",
+    password: "the-password",
+    ultimo_acceso: 1,
+    puede_entrar: true
+  },
+  "test-role-1": {
+    nombre: "test-role",
+    slug: "test-role"
+  },
+  "have-roles": {
+    login: "have-roles",
+    password: "the-password",
+    ultimo_acceso: 1,
+    puede_entrar: true
+  },
+  "test-role-2": {
+    nombre: "test-role-2",
+    slug: "test-role-2"
+  }
+};
--- server/api/Usuario/Usuario.spec.js
+++ server/api/Usuario/Usuario.spec.js
@@ -4,6 +4,7 @@ var UsuarioFacade = require("./Usuario.model.js");
 var Usuario = UsuarioFacade.Usuario;
 var Role = UsuarioFacade.Role;
 var Promise = require("bluebird");
+var join = Promise.join;
 
 var should = require('should');
 var app = require('../../app');
@@ -14,20 +15,22 @@ var expect = require('chai').expect;
 
 var sequelize_error = require('sequelize/lib/errors.js');
 
+/** CONSTANTES **/
+
+var FIXTURES = require("./Usuario.fixtures.js");
+
+/** FIN: CONSTANTES **/
+
 /*** AUXILIARES **/
 
 function login_as_admin(request){
   return new Promise(function(resolve, reject){
-    var auth_data = {
-      login: "admin", 
-      password: "admin"
-    };
 
     var credentials = {token: null}
 
     request(app)
       .post("/auth/local")
-      .send(auth_data)
+      .send(FIXTURES["admin-credentials"])
       .expect(200)
       .end(function(err, res){
         if(err){
@@ -63,13 +66,8 @@ describe('GET /api/auth', function() {
   });
 
   it('should make crud', function(done){
-    var body = {
-      login: "test1",
-      password: "the-password",
-      ultimo_acceso: 1,
-      puede_entrar: true
-    }
-    Usuario.create(body)
+
+    Usuario.create(FIXTURES["test-1"])
       .then(function(usuario){
         usuario.puede_entrar = false;
         return usuario.save();
@@ -77,7 +75,7 @@ describe('GET /api/auth', function() {
       .then(function(){
         return Usuario.findOne({
           where: {
-            login: body.login
+            login: FIXTURES["test-1"].login
           }
         });
       })
@@ -97,9 +95,6 @@ describe('GET /api/auth', function() {
   });
 
   it('should authenticate', function(done){
-    var credentials = {
-      token: null
-    };
 
     login_as_admin(request)
       .then(function(credentials){
@@ -115,10 +110,7 @@ describe('GET /api/auth', function() {
   });
 
   it("should autenticate (direct)", function(done){
-    var admin = Usuario.build({
-      login: "admin-2", 
-      password: "admin"
-    });
+    var admin = Usuario.build(FIXTURES["admin-2"]);
     admin.save()
       .then(function(admin){
         should(admin.autenticar("admin")).equal(true);
@@ -134,24 +126,16 @@ describe('GET /api/auth', function() {
 
     login_as_admin(request).then(function(credentials){
 
-      var data = {
-        login: "test2",
-        password: "the-password",
-        ultimo_acceso: 1,
-        puede_entrar: true
-      };
-
       request(app)
         .post("/api/auth/usuarios")
         .set("Authorization", "Bearer " + credentials.token)
-        .send(data)
+        .send(FIXTURES["test-2"])
         .expect(201)
         .end(function(err, res){
           if (err) {
             console.log(err);
             return done(err);
           } else {
-            data.should.be.instanceof(Object);
             res.body.should.be.instanceof(Object);
             done();
           }
@@ -162,19 +146,38 @@ describe('GET /api/auth', function() {
     });
   });
 
+  it("Role: CRUD", function(done){
+
+    Role.create(FIXTURES["test-role-2"])
+      .then(function(role){
+        role.nombre = "role-nombre-cambiado";
+        role.slug = null;
+        return role.save();
+      }).then(function(role){
+        role.nombre.should.be.eql("role-nombre-cambiado");
+        role.slug.should.be.eql("role-nombre-cambiado");
+        return role;
+      }).then(function(role){
+        return role.destroy();
+      }).then(function(){
+        done();
+      }).catch(function(error){
+        done(error);
+      });
+
+  });
+
   it("can eager load roles", function(done){
     Usuario.findOne({
       where: {login: 'admin'},
       include: {model: Role, as: "roles_full"}
-    })
-      .then(function(admin){
-        admin["roles_full"].should.be.instanceof(Array);
-        admin["roles_full"].length.should.be.equal(4);
-        return done();
-      })
-      .catch(function(err){
-        return done(err);
-      });
+    }).then(function(admin){
+      admin["roles_full"].should.be.instanceof(Array);
+      admin["roles_full"].length.should.be.equal(4);
+      return done();
+    }).catch(function(err){
+      return done(err);
+    });
   });
 
   it("can find with roles", function(done){
@@ -194,7 +197,7 @@ describe('GET /api/auth', function() {
 
     Role.find({
       where: {
-        slug: "test-role"
+        slug: FIXTURES["test-role-1"].slug
       }
     }).then(function(role){
       if(role)
@@ -202,16 +205,14 @@ describe('GET /api/auth', function() {
       else
         return;
     }).then(function(){
-      return Role.create({
-        nombre: "test-role"
-      });
+      return Role.create(FIXTURES["test-role-1"]);
     }).then(function(rol){
       return Usuario.find({
         where: {
           login: "admin"
         }
       }).then(function(admin){
-        return admin.anyadirRole("test-role");
+        return admin.anyadirRole(FIXTURES["test-role-1"].slug);
       });
 
     }).then(function(){
@@ -220,13 +221,13 @@ describe('GET /api/auth', function() {
           login: "admin"
         }
       }).then(function(admin){
-        expect(admin.roles).to.include("test-role");
+        expect(admin.roles).to.include(FIXTURES["test-role-1"].slug);
 
         // borrar el rol de prueba para que no afecte 
         // a la cuenta de roles por defecto.
         return Role.find({
           where: {
-            slug: "test-role"
+            slug: FIXTURES["test-role-1"].slug
           }
         }).then(function(role){
           return role.destroy();
@@ -237,6 +238,7 @@ describe('GET /api/auth', function() {
       });
 
     });
+
   });
 
 });
@@ -257,12 +259,8 @@ describe('GET /api/usuarios/roles', function() {
 
   it("assign roles to user", function(done){
 
-    Usuario.create({
-        login: "have-roles",
-        password: "the-password",
-        ultimo_acceso: 1,
-        puede_entrar: true
-      }).then(function(usuario){
+    Usuario.create(FIXTURES["have-roles"])
+      .then(function(usuario){
         return usuario.anyadirRole("profesor")
           .then(function(){
             return usuario;
@@ -271,9 +269,113 @@ describe('GET /api/usuarios/roles', function() {
         return usuario.reload();
       }).then(function(usuario){
         usuario.roles.length.should.be.equal(1);
+        return usuario.destroy();
+      }).then(function(){
         done();
       }).catch(function(error){
         done(error);
       });
+
   });
+
+  var RoleTesting = {
+    // "clase" auxiliar para crear dependencias para el test
+
+    createUsers: function(){
+      // () -> (Usuario x Usuario x Usuario)
+      return join(
+        Usuario.create(FIXTURES["usuario-1"]),
+        Usuario.create(FIXTURES["usuario-2"]),
+        Usuario.create(FIXTURES["usuario-3"])
+      ).spread(function(usuario1, usuario2, usuario3){
+        // les asignamos roles
+        usuario1.anyadirRole("profesor");
+        usuario2.anyadirRole("alumno");
+        usuario3.anyadirRole("profesor");
+
+        return join(usuario1, usuario2, usuario3);
+      });
+    },
+    destroyUsers: function(){
+      return join(
+        Usuario.destroy({where:{slug: FIXTURES["usuario-1"].slug } }),
+        Usuario.destroy({where:{slug: FIXTURES["usuario-2"].slug } }),
+        Usuario.destroy({where:{slug: FIXTURES["usuario-3"].slug } })
+      );
+    }
+  }
+
+  it("can delete roles whitout affecting users", function(done){
+
+    Role.create(FIXTURES["test-role-2"])
+      .then(function(role){
+        
+        return RoleTesting.createUsers()
+          .spread(function(usuario1, usuario2, usuario3){
+
+            return join(
+              // anyadir el Rol creado a los usuarios
+              usuario1.anyadirRole(role.slug),
+              usuario2.anyadirRole(role.slug),
+              usuario3.anyadirRole(role.slug)
+            ).then(function(){
+              // verificamos que tenemos los tres usuarios asignados al rol
+              return role.getUsuarios().then(function(usuarios){
+                usuarios.length.should.equal(3);
+              });
+
+            }).then(function(){
+              return join(
+                usuario1.reload(),
+                usuario2.reload(),
+                usuario3.reload()
+              );
+            }).then(function(){
+              usuario1.roles.should.eql(["test-role-2", "profesor"].sort());
+              usuario2.roles.should.eql(["test-role-2", "alumno"].sort());
+              usuario3.roles.should.eql(["test-role-2", "profesor"].sort());             
+            }).then(function(){
+              // destruimos el rol
+              return role.destroy();
+            }).then(function(){
+              // probamos que existen los usuarios
+              return join(
+                  Usuario.count({where: {slug: usuario1.slug}}),
+                  Usuario.count({where: {slug: usuario2.slug}}),
+                  Usuario.count({where: {slug: usuario3.slug}})
+                ).spread(function(count1, count2, count3){
+                  count1.should.be.equal(1);
+                  count2.should.be.equal(1);
+                  count3.should.be.equal(1);
+                });
+            }).then(function(){
+              // verificar los roles que tienen asignados los usuarios
+              return join( // actualizamos
+                  usuario1.reload(),
+                  usuario2.reload(),
+                  usuario3.reload()
+                ).spread(function(usuario1, usuario2, usuario3){
+                  // verificamos que tengan solo los roles originales asignados al crear
+                  usuario1.roles.should.be.eql(["profesor"]);
+                  usuario2.roles.should.be.eql(["alumno"]);
+                  usuario3.roles.should.be.eql(["profesor"]);
+
+                  return join(usuario1, usuario2, usuario3);
+                });
+            });
+
+          }).then(function(){
+            return RoleTesting.destroyUsers();
+          });
+
+      }).then(function(role){
+        //return role.destroy();
+      }).then(function(){
+        done();
+      }).catch(function(error){
+        done(error);
+      });
+
+  });
+  
 });
