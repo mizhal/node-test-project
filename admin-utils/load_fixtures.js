d6c6d928cd1bd64c6374461075d52cfad4077cce
mizhal <mizhal@gmail.com>
2015-07-21 11:25:06 +0200

    Fixtures con datos de alumnos y profesores

--- admin-utils/load_fixtures.js
+++ admin-utils/load_fixtures.js
@@ -10,5 +10,12 @@ var envconfig = require('../server/config/database.json');
 var sequelize_fixtures = require('sequelize-fixtures');
 
 //models
+var Usuarios = require("../server/api/Usuario/Usuario.model.js");
+
+// disable temporarily validation hooks (to avoid password generation)
+Usuarios.Usuario.options.hooks['beforeValidate'] = null
+
+sequelize_fixtures.loadFile("fixtures/usuarios.fixtures.yml", Usuarios);
+
 var Cursos = require("../server/api/cursos/cursos.model.js");
 sequelize_fixtures.loadFile("fixtures/cursos.fixtures.yml", Cursos);
